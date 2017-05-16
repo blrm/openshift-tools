c9d31549203b29a62e74a9fc812ebe649a5d7446
Blair Morrison <bmorriso@redhat.com>
2017-05-16 14:57:56 -0400

    update usage-pv check to handle SI units correctly

--- scripts/monitoring/cron-send-usage-pv.py
+++ scripts/monitoring/cron-send-usage-pv.py
@@ -110,16 +110,20 @@ def get_pv_usage_clusterresourcequota():
 
 def convert_to_bytes(data):
     """convert to bytes"""
-    (number, unit) = re.search("([0-9]+)([A-Za-z]+)", data.strip()).groups()
+    storage_units = {
+        ''  : 1,
+        'Ki': 1024,
+        'Mi': 1048576,
+        'Gi': 1073741824,
+        "k" : 1000,
+        "M" : 1000000,
+        "G" : 1000000000,
+    }
 
-    if unit.lower() == 'bi':
-        return int(number)
-    elif unit.lower() == 'ki':
-        return int(number) * 1024
-    elif unit.lower() == 'mi':
-        return int(number) * 1024 * 1024
-    elif unit.lower() == 'gi':
-        return int(number) * 1024 * 1024 * 1024
+    (number, unit) = re.search("([0-9]+)([A-Za-z]*)", data.strip()).groups()
+
+    if unit in storage_units:
+        return int(number) * storage_units[unit]
 
     raise Exception("invalid input data: " + data)
 
